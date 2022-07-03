f7adf7945229a2a485d48797acc5312e8292469b
rajesh.kumar@goavega.com-at-012114828737 <rajesh.kumar@goavega.com>
2022-07-03 08:47:24 +0530

    task-2 completed!

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,14 @@
-const fs = require("fs");
-const data = `   Hello Node js I am coming for you!  `;
-fs.appendFile("notes.txt", data, (err) => {
-  if (err) throw err;
-  console.log("The file is created if not existing!!");
-});
+// task 1
+
+// const fs = require("fs");
+// const data = `   Hello Node js I am coming for you!  `;
+// fs.appendFile("notes.txt", data, (err) => {
+//   if (err) throw err;
+//   console.log("The file is created if not existing!!");
+// });
+
+// task 2
+
+const getFunc = require("./notes");
+
+console.log(getFunc());
