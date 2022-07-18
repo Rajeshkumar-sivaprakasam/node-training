d3047a0d830c486d3fd8a8707aa31636eb1e239d
rajesh.kumar@goavega.com-at-012114828737 <omAQZCpcWPwd4hRX5oJ8PtZh9+6+lM1W3mI5Gv6pHng=>
2022-07-18 08:26:52 +0530

    json-server configured!

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,17 +1,21 @@
 const request = require("postman-request");
-
+const api = require("./utils/apiservice");
 const uri =
-  "http://api.weatherstack.com/current?access_key=df806e3b94a3a7e94fa60bbf0ce25254&query=New%20York&units=f";
+	"http://api.weatherstack.com/current?access_key=df806e3b94a3a7e94fa60bbf0ce25254&query=New%20York&units=f";
 request(uri, { json: true }, (error, response) => {
-  // const res = JSON.parse(response.body); //if you didn't use json param use this way
-  // console.log(res.current);
+	// const res = JSON.parse(response.body); //if you didn't use json param use this way
+	// console.log(res.current);
 
-  // console.log(response.body.current);
-  const res = response.body;
-  console.log(
-    `It is currently ${res.current.temperature} degrees out. It feels like ${res.current.precip} degrees out.`
-  );
+	// console.log(response.body.current);
+	const res = response.body;
+	console.log(
+		`It is currently ${res.current.temperature} degrees out. It feels like ${res.current.precip} degrees out.`
+	);
 });
-const geocode = () => {
-  console.log();
+
+//dummy json-api-mapbox
+const geocode = async () => {
+	const responseMapbox = await api.get("/data");
+	console.log(responseMapbox.data, "responseMapbox");
 };
+geocode();
