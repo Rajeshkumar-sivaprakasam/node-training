16453f16cf2e91244db29274ac22a2a21b0a2d8a
rajesh.kumar@goavega.com-at-012114828737 <omAQZCpcWPwd4hRX5oJ8PtZh9+6+lM1W3mI5Gv6pHng=>
2022-07-25 06:43:40 +0530

    about returning html && weather returning a object!

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,14 +3,23 @@ const express = require("express");
 const app = express();
 
 app.get("", (req, res) => {
-	console.log("Hello Express!");
 	res.send("Hello Express!");
 });
 
 //about
 app.get("/about", (req, res) => {
-	console.log("You are in about page!");
-	res.send("You are in about page!");
+	res.send("<h1>About Page!</h1>");
+});
+
+//weather
+app.get("/weather", (req, res) => {
+	res.send({
+		forcast: "cool",
+		location: {
+			longitude: 20.89,
+			latitude: -36.67,
+		},
+	});
 });
 
 app.listen(3000, () => {
