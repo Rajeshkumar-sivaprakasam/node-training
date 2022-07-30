3375968a563340a6b8a96ec933c5c461d25ab703
rajesh.kumar@goavega.com-at-012114828737 <omAQZCpcWPwd4hRX5oJ8PtZh9+6+lM1W3mI5Gv6pHng=>
2022-07-30 11:22:07 +0530

    await challange completed!

--- task-manager/playground/promise.js
+++ task-manager/playground/promise.js
@@ -1,14 +1,28 @@
 require("../db/mongoose");
-
+const User = require("../model/user");
 const Task = require("../model/task");
 
-Task.findByIdAndDelete("62e1fa28639e432be28b9340")
-  .then((result) => {
-    return Task.countDocuments({ isCompleted: true });
-  })
-  .then((result) => {
-    console.log(result);
+// Task.findByIdAndDelete("62e1fa28639e432be28b9340")
+//   .then((result) => {
+//     return Task.countDocuments({ isCompleted: true });
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+const deleteTaskAndCount = async (id) => {
+  const task = Task.findByIdAndDelete(id);
+  const count = Task.countDocuments({ isCompleted: true });
+  return count;
+};
+
+deleteTaskAndCount("62e4c09d07de6518aff9efc4")
+  .then((count) => {
+    console.log(count, "Count");
   })
   .catch((e) => {
-    console.log(e);
+    console.log("e", e);
   });
