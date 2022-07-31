e94bdf9e82ab332acbea0989e0fb8e009551703a
rajesh.kumar@goavega.com-at-012114828737 <omAQZCpcWPwd4hRX5oJ8PtZh9+6+lM1W3mI5Gv6pHng=>
2022-07-31 13:03:12 +0530

    express middlewere added

--- task-manager/index.js
+++ task-manager/index.js
@@ -7,6 +7,17 @@ require("./db/mongoose");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.send("Get service is disabled!");
+//   } else {
+//     next();
+//   }
+// });
+
+app.use((req, res, next) => {
+  res.status(503).send("Site is currently down!");
+});
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
