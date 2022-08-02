e3aa620e7a331f27178ffec6060796d1f6529479
rajesh.kumar@goavega.com-at-012114828737 <omAQZCpcWPwd4hRX5oJ8PtZh9+6+lM1W3mI5Gv6pHng=>
2022-08-02 08:12:35 +0530

    sorting added!

--- task-manager/routers/tasks.js
+++ task-manager/routers/tasks.js
@@ -5,11 +5,18 @@ const auth = require("../middleware/auth");
 
 //GET /tasks ? completed= true
 //GET /tasks ? limit= 2 & skip=2
+//GET /tasks ? sortBy=createdAt:desc
 
 router.get("/tasks", auth, async (req, res) => {
   const match = {};
+  const sort = {};
+
   if (req.query.completed) {
     match.completed = req.query.completed === "true";
+  }
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
   }
   try {
     // const tasks = await Task.find({});
@@ -20,6 +27,7 @@ router.get("/tasks", auth, async (req, res) => {
         options: {
           limit: parseInt(req.query.limit),
           skip: parseInt(req.query.skip),
+          sort,
         },
       })
       .execPopoulate();
